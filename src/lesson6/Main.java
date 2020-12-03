13b7ed19475496d1a8e1b34518adbac7ae4d6ca5
lagutin_vlad <lagutin_vlad@mail.ru>
2020-12-03 21:21:12 +0300

    hw6

--- src/lesson6/Main.java
+++
@@ -1,6 +0,0 @@
-package lesson6;
-
-
-public class Main {
-
-}
--- src/lesson6/animals/Cat.java
+++ src/lesson6/animals/Cat.java
@@ -1,4 +1,15 @@
 package lesson6.animals;
 
-public class Cat {
+public class Cat extends Animals {
+
+    public Cat(String name) {
+        this(name, 0, 200);
+    }
+
+    public Cat(String name, int swimLimit, int runLimit) {
+        super(name);
+        this.swimLimit = swimLimit;
+        this.runLimit = runLimit;
+    }
+
 }
