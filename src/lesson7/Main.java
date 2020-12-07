fe5fc256d3851670966edafe9ac81915ab302c6b
lagutin_vlad <lagutin_vlad@mail.ru>
2020-12-07 18:15:51 +0300

    hw6

---
+++ src/lesson7/Main.java
@@ -0,0 +1,16 @@
+package lesson7;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] arr = new int[10];
+//        int e = new int[4][8].length;
+//        System.out.println(e);
+//        String a = "java";
+//        System.out.println(a.toUpperCase());
+        [] int [] a = new int[5][5];
+        System.out.println(Arrays.toString(a));
+
+    }
+}
